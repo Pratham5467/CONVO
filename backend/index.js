91ee225351fb3870a918d2544b4e38caeb097e16
Pratham Srivastava <118266497+Pratham5467@users.noreply.github.com>
2025-08-07 14:48:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -116,4 +116,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(5000,()=> console.log('Server started on port 5000'));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
